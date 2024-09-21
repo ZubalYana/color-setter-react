345e5c28caee215588ba56f6a90c4367a3139d3f
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-21 15:28:27 +0300

    display header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
-
+import Header from './components/Header/Header.jsx'
 function App() {
 
   return (
-    <div className="wrap"></div>
+    <div className="wrap">
+      <Header />
+    </div>
   )
 }
 
