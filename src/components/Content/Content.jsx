c71a361c34f637911072af2eba9b9d75212227a1
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-21 15:51:47 +0300

    extend the content lorem

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,7 +4,7 @@ export default function Content() {
 
     return (
         <div className="Content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum et nobis eligendi vitae fugiat nam consectetur dolores sed, enim expedita, veniam facilis ullam aut doloremque odio mollitia incidunt rerum soluta, a minus eius quidem aperiam ea? Atque amet rem nulla rerum distinctio, laboriosam repudiandae reiciendis dignissimos quibusdam asperiores, et totam?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex assumenda voluptatum dignissimos odit, laboriosam ipsa doloremque enim, dicta illo ut omnis nihil maiores inventore quod ullam eveniet iusto placeat! Aliquam delectus sit modi, ut quas dolores amet quibusdam reiciendis repellat hic facilis beatae fuga maiores non possimus odit neque quos voluptatem! Incidunt a accusantium nemo corrupti consequatur ipsam, architecto nihil vero, exercitationem et itaque maxime impedit dolorum veritatis! Molestias illum quas facere velit nostrum atque magnam, ipsa deserunt nesciunt doloremque in? Voluptatibus nemo explicabo, in praesentium eligendi fuga optio ratione deleniti assumenda. Doloribus error accusamus nisi blanditiis, enim consectetur atque?</p>
         </div>
     )
 }
