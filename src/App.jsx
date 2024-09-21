5d62bcb92d5db5702dce444492d6846b7d64b836
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-21 15:48:24 +0300

    display and style content

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="wrap">
       <Header />
+      <Content />
     </div>
   )
 }
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -4,7 +4,7 @@ export default function Content() {
 
     return (
         <div className="Content">
-            <p></p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum et nobis eligendi vitae fugiat nam consectetur dolores sed, enim expedita, veniam facilis ullam aut doloremque odio mollitia incidunt rerum soluta, a minus eius quidem aperiam ea? Atque amet rem nulla rerum distinctio, laboriosam repudiandae reiciendis dignissimos quibusdam asperiores, et totam?</p>
         </div>
     )
 }
